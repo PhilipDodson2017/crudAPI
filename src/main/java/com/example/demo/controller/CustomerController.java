fefa9990a47c7b192b784154591fbd5f7940646e
Philip Dodson <philipdodson@philips-mbp.mynetworksettings.com>
2024-12-15 18:24:02 -0500

    Add createCustomer and getAllCustomers endpoints and fix ApplicationContext error with CustomControllerTest

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,8 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Customer;
+import com.example.demo.repository.CustomerRepository;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,9 +22,29 @@ import org.apache.logging.log4j.Logger;
 public class CustomerController {
     private static final Logger logger = LogManager.getLogger(CustomerController.class);
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @GetMapping("/hello")
     public String sayHello() {
         logger.info("Testing log4j, Hello World!");
         return "Testing testing, Hello World!";
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        logger.info("Creating a new customer: {}", customer);
+        Customer savedCustomer = customerRepository.save(customer);
+        logger.debug("Customer created with ID: {}", savedCustomer.getId());
+        return ResponseEntity.ok(savedCustomer);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        logger.info("Fetching all customers");
+        List<Customer> customers = customerRepository.findAll();
+        logger.debug("Number of customers fetched: {}", customers.size());
+        return ResponseEntity.ok(customers);
     }
 }
